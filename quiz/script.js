f2051f76ab49243580727454d25ac9f53606d39c
Leo <leocsdev@gmail.com>
2021-01-11 21:03:40 +0800

    Added instructions.

--- quiz/script.js
+++ quiz/script.js
@@ -1,1 +1,44 @@
-// console.log('Hi from script.js')
+
+
+
+
+
+
+/*
+
+Build a function constructor called Question to describe a question. A question should include:
+  - Question itself
+  - The answers from which the player can choose the correct one (array)
+  - Correct answer (I would use a number for this)
+  - Use this quiz as the source: https://www.proprofs.com/quiz-school/story.php?title=html-basics-quiz_1
+  - Store them all inside an array.
+
+  - Select one random question and log it on the console, together with the selection of answers (each question should have a number) (Hint: write a function for the Question objects for this task)
+  - Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer.
+  - Check if the answer is correct and print to the console whether the answer is correct or not (Hint: write another method for this)
+
+  Non-IIFE
+  let hiFunction = function() {
+      return {
+          sayHello() {
+              return "Hello!";
+          },
+          sayHi() {
+              return "Hi!";
+          }
+      };
+  };
+
+  IIFE
+  let hiFunction = (function() {
+      return {
+          sayHello() {
+              return "Hello!";
+          },
+          sayHi() {
+              return "Hi!";
+          }
+      };
+  })();
+
+*/
